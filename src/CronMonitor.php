43d3f6cadeea055dca405cab5b45196bafa1a9d4
zsluder@omnispear.com <zsluder@omnispear.com>
2018-04-26 16:55:15 -0400

    began validation

--- src/CronMonitor.php
+++ src/CronMonitor.php
@@ -12,7 +12,7 @@ class CronMonitor
 
     public function __construct()
     {
-        $this->config = include ('config.php');
+        $this->config = include('config.php');
 
         $this->client = new Client([
             'base_uri' => $this->config['CRONMONITOR_API'],
@@ -22,44 +22,47 @@ class CronMonitor
     /**
      * Notify API an execution has started
      */
-    public function sendStarted($environment, $task_id)
+    public function sendStarted($environment, $task_uuid)
     {
-        $execution_id = $this->client->request('POST', 'started' ,[
+        $response = json_decode($this->client->request('POST', 'started', [
             'form_params' => [
-                'task_id' => $task_id,
+                'task_id' => $task_uuid,
                 'environment' => $environment,
                 'started' => Carbon::now()->toDateTimeString()
             ]
-        ])->getBody()->getContents();
+        ])->getBody()->getContents());
 
-        return $execution_id;
+        if ($response->status !== 200) {
+
+        }
+
+        return $response->uuid;
     }
 
     /**
      * Notify API an execution has ended
      */
-    public function sendEnded($execution_id)
+    public function sendEnded($execution_uuid)
     {
-        $this->client->request('POST', 'ended' ,[
+        $this->client->request('POST', 'ended', [
             'form_params' => [
-                'execution_id' => $execution_id,
+                'execution_uuid' => $execution_uuid,
                 'ended' => Carbon::now()->toDateTimeString()
             ]
-        ])->getBody()->getContents();
+        ]);
     }
 
     /**
      * Notify API an execution has resulted in error
      */
-    public function sendError($execution_id, $error)
+    public function sendError($execution_uuid, $error)
     {
-        $this->client->request('POST', 'error' ,[
+        $this->client->request('POST', 'error', [
             'form_params' => [
-                'execution_id' => $execution_id,
+                'execution_id' => $execution_uuid,
                 'error' => $error,
                 'ended' => Carbon::now()->toDateTimeString()
             ]
-        ])->getBody()->getContents();
+        ]);
     }
-
 }
